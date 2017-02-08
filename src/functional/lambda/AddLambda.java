7162901bc961955ddee42ec94961b27346cbf3c0
kreest <kreestster@gmail.com>
2017-02-08 09:54:09 +0100

    corretta lambda

--- src/functional/lambda/AddLambda.java
+++ src/functional/lambda/AddLambda.java
@@ -1,8 +1,19 @@
 package functional.lambda;
-import functional.sam.Sam;
+
+import java.util.function.BinaryOperator;
 
 public class AddLambda {
 	public static void main(String...strings) {
-		int result = a -> a + b;
+		
+		
+		double result = operation((Double a, Double b)-> a + b, 1.0, 2.0);
+		
+		System.out.println(result);
+	}
+	
+	private static Double operation(BinaryOperator<Double> p,Double a, Double b) {
+		Double result = new Double(0);
+			result =p.apply(a, b);
+		return result;
 	}
 }
